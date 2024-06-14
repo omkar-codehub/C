f8e3a0bb25acd42760991ce0f18cdb6bc92448c9
Omkar <themoreomkar@gmail.com>
2024-06-14 21:15:54 +0530

    learnt compilation process

--- 01. Basics of C/StructcureOfC.c
+++ 01. Basics of C/StructcureOfC.c
@@ -2,6 +2,17 @@
 
 #include<stdio.h> //Preprocessor section
 
+#define PI 3.14 //definition section
+
+void area(int radius);    //Global declaration section
+
 int main(){ // main section
-    //main body
+   int radius = 5; //main body
+   area(radius);
+   return 0;
+}
+
+void area(int radius){      //Sub program section
+    float area = PI * radius * radius;
+    printf("Area of circle is %f",area);
 }
